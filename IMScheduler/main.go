99bbb5063e7677c4171713acb8c195557cc7eb4f
kerbalwzy <kerbalwzy@gmail.com>
2019-08-07 11:35:01 +0800

    rename AuthCenter to Usercenter

--- IMScheduler/main.go
+++ IMScheduler/main.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
-	"rouzhuo"
+	"net/http"
 )
 
-func init()  {
-	log.SetFlags(log.LstdFlags|log.Llongfile)
-	log.SetPrefix("IMServer ")
+func init() {
+	log.SetPrefix("IMScheduler")
 }
 
 func main() {
-	manager := &rouzhuo.Manager{ApiAddress:"127.0.0.1:8080"}
-	manager.StartApiServer()
+	//2 指定目录的静态文件
+	http.Handle("/asset/", http.FileServer(http.Dir(".")))
+	http.Handle("/mnt/", http.FileServer(http.Dir(".")))
+	RegisterView()
+	http.ListenAndServe(":8080", nil)
+}
+
+func RegisterView() {
+	//一次解析出全部模板
+	tpl, err := template.ParseGlob("view/**/*")
+	if nil != err {
+		log.Fatal(err)
+	}
+	//通过for循环做好映射
+	for _, v := range tpl.Templates() {
+		//
+		tplname := v.Name();
+		fmt.Println("HandleFunc     " + v.Name())
+		http.HandleFunc(tplname, func(w http.ResponseWriter,
+			request *http.Request) {
+			//
+			fmt.Println("parse     " + v.Name() + "==" + tplname)
+			err := tpl.ExecuteTemplate(w, tplname, nil)
+			if err != nil {
+				log.Fatal(err.Error())
+			}
+		})
+	}
+
 }
--- MsgCenter/main.go
+++ MsgCenter/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+	"ruozhuo"
+)
+
+func init() {
+	log.SetPrefix("MsgCenter ")
+}
+
+func main() {
+	worker := &ruozhuo.Worker{
+		Tag:          "msg",
+		Address:      "127.0.0.1:10000",
+		ManagerAdder: "127.0.0.1:7070",
+	}
+	ruozhuo.StartWorker(worker)
+}
