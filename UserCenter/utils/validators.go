8b70e45c7a61aaa03ef63580fa10330bb3eab89f
kerbalwzy <kerbalwzy@gmail.com>
2019-08-10 17:15:24 +0800

    create custom validators

--- UserCenter/utils/validators.go
+++ UserCenter/utils/validators.go
@@ -1,1 +1,81 @@
 package utils
+
+import (
+	"gopkg.in/go-playground/validator.v8"
+	"reflect"
+)
+
+func NameValidator(
+	v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
+	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
+) bool {
+	// Require the type is String and contains 1 to 10 characters.
+	if fieldKind != reflect.String {
+		return false
+	}
+	value := field.String()
+	if value == "" || len(value) > 10 {
+		return false
+	}
+	return true
+}
+
+func MobileValidator(
+	v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
+	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
+) bool {
+	// Require the type is String and contains 11 numeric characters.
+	if fieldKind != reflect.String {
+		return false
+	}
+	value := field.String()
+	if len(value) != 11 {
+		return false
+	}
+	return validator.IsNumeric(v, topStruct, currentStructOrField, field, fieldType, fieldKind, param)
+}
+
+func EmailValidator(
+	v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
+	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
+) bool {
+	// Require the type is String and length less than 100, and Email format.
+	if fieldKind != reflect.String {
+		return false
+	}
+	value := field.String()
+	if len(value) > 100 {
+		return false
+	}
+	return validator.IsEmail(v, topStruct, currentStructOrField, field, fieldType, fieldKind, param)
+}
+
+func PasswordValidator(
+	v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
+	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
+) bool {
+	// Require the type is String, and length between 8 to 12.
+	if fieldKind != reflect.String {
+		return false
+	}
+	value := field.String()
+	if len(value) < 8 || len(value) > 12 {
+		return false
+	}
+	return true
+}
+
+func GenderValidator(
+	v *validator.Validate, topStruct reflect.Value, currentStructOrField reflect.Value,
+	field reflect.Value, fieldType reflect.Type, fieldKind reflect.Kind, param string,
+) bool {
+	// Require the type is Int, and value is -1 or 0 or 1
+	if fieldKind != reflect.Int {
+		return false
+	}
+	value := field.Int()
+	if value < -1 || value > 1 {
+		return false
+	}
+	return true
+}
