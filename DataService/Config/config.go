caf1a03812eba5c5267fe383c911cb4afc30d9f4
kerbalwzy <kerbalwzy@gmail.com>
2019-09-26 11:10:05 +0800

    Update: update config

--- DataService/Config/config.go
+++ DataService/Config/config.go
@@ -1,15 +1,24 @@
 package Config
 
 const (
-	MySQLDataRPCServerAddress = "0.0.0.0:23331"
-	MongoDataRPCServerAddress = "0.0.0.0:23332"
+	MySQLURI = "root:123456@tcp(127.0.0.1:3306)/PrivateIM?charset=utf8&parseTime=true"
 
-	UserDbMySQLURI = "root:123456@tcp(127.0.0.1:3306)/IMUserCenter?charset=utf8" +
-		"&parseTime=true"
-
-	MsgDbMongoURI = "mongodb://localhost:27017"
+	MongoDBURI                     = "mongodb://localhost:27017"
+	MongoDBName                    = "privateIM"
+	CollDelayMessageName           = "delay_message"
+	CollChatHistoryName            = "user_chat_history"
+	CollGroupChatHistoryName       = "group_chat_history"
+	CollSubscriptionMsgHistoryName = "subscription_msg_history"
+	CollUserFriendsName            = "user_friends"
+	CollUserGroupChatsName         = "user_group_chats"
+	CollUserSubscriptions          = "user_subscriptions"
+	CollGroupChatUsersName         = "group_chat_users"
+	CollSubscriptionUsersName      = "subscription_users"
 
 	TimeDisplayFormat = "2006-01-02 15:04:05"
+
+	MySQLDataRPCServerAddress = "0.0.0.0:23331"
+	MongoDataRPCServerAddress = "0.0.0.0:23332"
 
 	DataLayerSrvCAPem       = "/Users/wzy/GitPrograms/PrivateIM/DataService/CATSL/ca.pem"
 	DataLayerSrvCAServerPem = "/Users/wzy/GitPrograms/PrivateIM/DataService/CATSL/server/server.pem"
